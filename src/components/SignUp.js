8f0d602c6be0611f2fda158e573fda61ab152950
Nogradi123 <nogradi.nicholas1@gmail.com>
2022-11-19 18:02:15 -0500

    added user authentication

---
+++ src/components/SignUp.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+export default function SignupOrLogin({action, getUserInfo}) {
+
+    const [formState, setFormState] = useState({
+        username: "",
+        password: ""
+    });
+
+    const updateInput = (e, thingToUpdate)=>{
+        setFormState({...formState, [thingToUpdate]: e.target.value})
+    }
+
+    
+    const submitSignupForm = () =>{
+        let endpoint;
+        if(action ==="signup") endpoint = "signup";
+        if(action ==="login") endpoint = "login"
+
+        axios.post("http://localhost:4200/"+endpoint, {
+            username: formState.username,
+            password: formState.password
+        },
+        {withCredentials: true}
+        )
+        .then((response)=>{
+            getUserInfo();
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+
+    }
+
+
+    return(
+
+    <div className={action}>
+        {action === "signup" ? "Signup" : "Login"}
+        <br />
+        <div>
+            <input type="text" value={formState.username} onChange={(e)=>{updateInput(e, "username")}} placeholder= " Enter Username" />
+        </div>
+        <div>
+            <input type="text" value={formState.password} onChange={(e)=>{updateInput(e, "password")}} placeholder="Enter Password"/>
+        </div>
+        <button onClick={submitSignupForm}>Submit</button>
+    </div>
+    )
+}
